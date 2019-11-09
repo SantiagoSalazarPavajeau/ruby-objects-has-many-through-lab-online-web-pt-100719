4b396028372b32ecfab1c767c892ab5776d5cd82
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-09 16:03:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,7 +19,7 @@ class Patient
   
   def new_appointment(doctor, date)
     appointment = Appointment.new(date, self, doctor)
-    self.appointment = appointment
+    self.appointments = appointment
   end
   
   def doctors
